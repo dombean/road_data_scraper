65b97479a7335980852dd43cea24981c6d36ea72
dombean <46692370+dombean@users.noreply.github.com>
2021-12-28 16:55:09 +0000

    Added Dockerfile and FastAPI Script for Google Cloud Run

---
+++ api_main.py
@@ -0,0 +1,53 @@
+from datetime import date
+from typing import Optional
+
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+
+from road_data_scraper import __version__
+from road_data_scraper.main import run
+
+app = FastAPI(
+    title="Road Data Scraper API",
+    description="Scrapes and Cleans WebTRIS Traffic Flow API",
+    version=__version__,
+)
+
+
+@app.get("/")
+def read_docs():
+    return RedirectResponse("/docs")
+
+
+@app.get("/scrape/", tags=["webtris"])
+def scrape_webtris_api(
+    start_date: date,
+    end_date: date,
+    test_run: bool,
+    generate_report: bool,
+    output_path: str,
+    rm_dir: bool,
+    gcp_storage: bool,
+    gcp_credentials: Optional[str] = None,
+    gcp_bucket_name: Optional[str] = None,
+    gcp_blob_name: Optional[str] = "landing_zone",
+):
+
+    config = {
+        "user_settings": {
+            "start_date": str(start_date),
+            "end_date": str(end_date),
+            "test_run": test_run,
+            "generate_report": generate_report,
+            "output_path": output_path,
+            "rm_dir": rm_dir,
+            "gcp_storage": gcp_storage,
+            "gcp_credentials": gcp_credentials,
+            "gcp_bucket_name": gcp_bucket_name,
+            "gcp_blob_name": gcp_blob_name,
+        }
+    }
+
+    run(config, api_run=True)
+
+    return "Success"
