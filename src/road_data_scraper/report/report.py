8302f86b8bc2ce233c4f67aa002cc593414d7eb5
dombean <46692370+dombean@users.noreply.github.com>
2023-06-01 12:27:16 +0100

    Better docstring for report.py

--- src/road_data_scraper/report/report.py
+++ src/road_data_scraper/report/report.py
@@ -1,7 +1,32 @@
+"""
+Reports Module
+--------------
+
+This module provides functionalities for generating HTML reports based on a template Jupyter notebook. It uses the Papermill and nbconvert libraries to execute the notebook and convert it to HTML format.
+
+Imported Libraries:
+-------------------
+- glob: For retrieving file paths matching a specified pattern.
+- logging: For logging progress and errors.
+- warnings: For managing warnings and filtering.
+- pathlib.Path: For handling file paths.
+- papermill: For executing Jupyter notebooks and passing parameters.
+- nbconvert.exporters.HTMLExporter: For exporting notebooks to HTML format.
+- nbconvert.preprocessors.TagRemovePreprocessor: For removing specific cells or tags from notebooks.
+- nbconvert.writers.FilesWriter: For writing notebook outputs.
+- traitlets.config.Config: For configuring nbconvert and its preprocessors.
+
+Global Variables:
+-----------------
+- c (Config): Configuration object for nbconvert and preprocessors.
+- exporter (HTMLExporter): Exporter object for converting notebooks to HTML format.
+"""
 import glob
 import logging
 import warnings
 from pathlib import Path
+
+import pandas as pd
 
 with warnings.catch_warnings():
     warnings.simplefilter(action="ignore", category=FutureWarning)
@@ -25,7 +50,47 @@ exporter = HTMLExporter(config=c)
 exporter.register_preprocessor(TagRemovePreprocessor(config=c), True)
 
 
-def run_reports(data, full_path, start_date, end_date):
+def run_reports(
+    data: pd.DataFrame, full_path: str, start_date: str, end_date: str
+) -> None:
+    """
+    Generates an HTML report by executing a template Jupyter notebook. The function populates the template with the provided data
+    and saves the generated report as an HTML file.
+
+    The function takes the following steps:
+    - Specifies the input path for the template notebook.
+    - Defines the output path for the generated report notebook.
+    - Sets up the parameters for the notebook execution, including the report title and data to be included.
+    - Executes the template notebook using Papermill, passing the parameters and enabling report mode.
+    - Converts the executed notebook to HTML format using nbconvert.
+    - Saves the HTML report to the specified output directory.
+
+    Note:
+    - The template notebook should have the appropriate placeholders to receive the data and generate the report.
+    - The data should be in a pandas DataFrame format.
+    - The full path provided should be an existing directory where the report will be saved.
+
+    Args:
+        data (pd.DataFrame): Data to be included in the report.
+        full_path (str): Full path of the directory where the report will be saved.
+        start_date (str): Start date of the data range used for the report.
+        end_date (str): End date of the data range used for the report.
+
+    Returns:
+        None
+
+    Example:
+    ```
+    data = pd.DataFrame(...)
+    full_path = "/path/to/reports"
+    start_date = "2022-01-01"
+    end_date = "2022-01-31"
+
+    run_reports(data, full_path, start_date, end_date)
+    ```
+
+    The above example generates an HTML report using the provided data and saves it in the specified directory.
+    """
 
     logging.info(f"Generating HTML Report at {full_path}")
 
